f25cebc6d6470d1186ba9868e6466b2288bd1d42
Annfeng <yfeng@broadinstitute.org>
2019-12-24 00:04:38 -0500

    add scripts

--- scripts/02_plot_snp_missdiff_btw_batch.sh
+++ scripts/02_plot_snp_missdiff_btw_batch.sh
@@ -25,31 +25,55 @@ Rscript $scrdir/02_plot_snp_misdiff_btw_batch.R $preqcdir $fbatchpr
 # for batch in MEGA  MEG_A1_A  MEG_A1_B  MEGAEX  MEG_C  MEG_D  MEG_E  MEG_X1; do 
     # wc -l $batch/pbk_geno05_mind02_geno02.lmiss
     # echo $batch
-    # head -2 $batch/qc_summary.tsv
+    # cat $batch/qc_summary.tsv
 # done
 
 
 # MEGA
 # Ninds 4924
 # Nsnps 1416020
+# Nsnps with call rate >0.95 1416016
+# Ninds with call rate >0.98 4924
+# Nsnps with call rate >0.98 1414935
 # MEG_A1_A
 # Ninds 4781
 # Nsnps 1778953
+# Nsnps with call rate >0.95 1768889
+# Ninds with call rate >0.98 4781
+# Nsnps with call rate >0.98 1754742
 # MEG_A1_B
 # Ninds 5020
 # Nsnps 1778953
+# Nsnps with call rate >0.95 1767259
+# Ninds with call rate >0.98 5020
+# Nsnps with call rate >0.98 1753275
 # MEGAEX
 # Ninds 5344
 # Nsnps 1741376
+# Nsnps with call rate >0.95 1740534
+# Ninds with call rate >0.98 5344
+# Nsnps with call rate >0.98 1736686
 # MEG_C
 # Ninds 5492
 # Nsnps 1778953
+# Nsnps with call rate >0.95 1764911
+# Ninds with call rate >0.98 5492
+# Nsnps with call rate >0.98 1751424
 # MEG_D
 # Ninds 5146
 # Nsnps 1778953
+# Nsnps with call rate >0.95 1767133
+# Ninds with call rate >0.98 5146
+# Nsnps with call rate >0.98 1749982
 # MEG_E
 # Ninds 4851
 # Nsnps 1778953
+# Nsnps with call rate >0.95 1767635
+# Ninds with call rate >0.98 4851
+# Nsnps with call rate >0.98 1750557
 # MEG_X1
 # Ninds 866
 # Nsnps 1778953
+# Nsnps with call rate >0.95 1768426
+# Ninds with call rate >0.98 866
+# Nsnps with call rate >0.98 1752499
